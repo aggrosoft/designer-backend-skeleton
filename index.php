1f04718d61eafe0d375e877d8f808b9001872e20
alexander <coding@aggrosoft.de>
2020-01-13 11:49:31 +0100

    missing slash for path

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Shirtnetwork\Backend\Server\Server;
 
-if (!file_exists(__DIR__ . 'config.inc.php')){
-    copy(__DIR__ . 'config.inc.sample.php', __DIR__ . 'config.inc.php');
+if (!file_exists(__DIR__ . '/config.inc.php')){
+    copy(__DIR__ . '/config.inc.sample.php', __DIR__ . '/config.inc.php');
 }
 
-$config = include __DIR__ . 'config.inc.php';
+$config = include __DIR__ . '/config.inc.php';
 $server = new Server($config);
 $server->serve();
