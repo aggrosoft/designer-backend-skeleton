60b9619274b01fd0e6f31ed4f3db2fdcf116bc6f
alexander <coding@aggrosoft.de>
2020-01-17 11:02:02 +0100

    Added console binary

--- bin/cron.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-use Shirtnetwork\Backend\Server\Server;
-
-$config = include __DIR__ . 'config.inc.php';
-$server = new Server($config);
-$server->serve();
