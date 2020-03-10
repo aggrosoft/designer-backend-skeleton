a6d0b62e23140902ca5ed0a64dc2fc9c924120b9
alexander <coding@aggrosoft.de>
2020-03-10 10:13:23 +0100

    allow config.inc.php to be named cfg.inc.php as well, nginx has problems with slim routing when a file exists with a route name

--- index.php
+++ index.php
@@ -4,11 +4,18 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Shirtnetwork\Backend\Server\Server;
 
-if (!file_exists(__DIR__ . '/config.inc.php')){
+$configFile = dirname(__FILE__) . '/../config.inc.php';
+$cfgFile = dirname(__FILE__) . '/../cfg.inc.php';
+
+if (!file_exists($configFile) && !file_exists($cfgFile)){
     copy(__DIR__ . '/config.inc.sample.php', __DIR__ . '/config.inc.php');
 }
 
-$config = include __DIR__ . '/config.inc.php';
+if (file_exists($configFile)) {
+    $config = include $configFile;
+}elseif(file_exists($cfgFile)) {
+    $config = include $cfgFile;
+}
 
 if ($config['debug'] === true){
     @ini_set("display_errors",1);
